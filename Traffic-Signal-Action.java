dffcd81312880bf874ebfb07afe4f07ba00e258a
HimajaVenkataReddy <HimajaVenkataReddy@users.noreply.github.com>
2026-09-26 22:02:28 +0530

    Add traffic signal timing

--- Traffic-Signal-Action.java
+++ Traffic-Signal-Action.java
@@ -13,10 +13,13 @@ public class TrafficSignalAction {
 
         if (signal.equalsIgnoreCase("red")) {
             System.out.println("Action: STOP");
+            System.out.println("Signal Duration: 60 seconds");
         } else if (signal.equalsIgnoreCase("yellow")) {
             System.out.println("Action: WAIT");
+            System.out.println("Signal Duration: 5 seconds");
         } else if (signal.equalsIgnoreCase("green")) {
             System.out.println("Action: GO");
+            System.out.println("Signal Duration: 45 seconds");
         } else {
             System.out.println("Invalid traffic signal color.");
         }
